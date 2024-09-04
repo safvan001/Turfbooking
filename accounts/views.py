d05a22f1826ef67aa4ae71cac5f16e2c56b6f12b
shameemm <shameem.n@bracha.in>
2024-09-04 06:30:24 +0530

    verify-otp

--- accounts/views.py
+++ accounts/views.py
@@ -30,13 +30,35 @@ class UserLoginView(APIView):
     
 
 class InitiateOtp(APIView):
+    """Initiate OTP for login"""
     def get(self,request, *args, **kwargs):
         phone_number = request.GET.get("phone_number",'')
-        print(phone_number)
         if User.objects.filter(phone_number=phone_number).exists():
             url = settings.TWO_FACTOR_BASE_URL
             api_key = settings.TWO_FACTOR_API_KEY
-            response = requests.get('${url}${api_key}SMS/+91${phone_number}/AUTOGEN/OTP1')
-            return Response(response.text)
+            response = requests.get(f'{url}/{api_key}/SMS/+91{phone_number}/AUTOGEN/OTP1')
+            return Response(response.json())
         else:
             return Response('user not found')
+    
+class VerifyOtp(APIView):
+    """Verify OTP for Login"""
+    def get(self, request, *args, **kwargs):
+        phone_number = request.GET.get("phone_number",'')
+        otp = request.GET.get("otp","")
+        if User.objects.filter(phone_number=phone_number).exists():
+            url = settings.TWO_FACTOR_BASE_URL
+            api_key = settings.TWO_FACTOR_API_KEY
+            response = requests.get(f'{url}/{api_key}/SMS/VERIFY3/91{phone_number}/{otp}')
+            if response.json()['Status'] == 'Success':
+                user = User.objects.get(phone_number=phone_number)
+                refresh=RefreshToken.for_user(user)
+                return Response({
+                    'access':str(refresh.access_token),
+                    'refrseh':str(refresh),
+                    'message':'User authenticated Successfully'
+                },status=status.HTTP_200_OK)
+            else:
+                return Response({'error':"OTP is not valid"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response('user not found', status=status.HTTP_400_BAD_REQUEST)
